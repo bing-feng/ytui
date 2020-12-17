0c25f2e9a2eab249e044a13e8c82538115178b89
yl <544738978@qq.com>
2020-12-17 15:17:49 +0800

    基础框架

--- app/src/main/java/com/winton/ytui/ui/home/HomeFragment.kt
+++ app/src/main/java/com/winton/ytui/ui/home/HomeFragment.kt
@@ -13,31 +13,16 @@ import com.winton.ytui.R
 
 class HomeFragment : Fragment() {
 
-    private lateinit var homeViewModel: HomeViewModel
-
     override fun onCreateView(
             inflater: LayoutInflater,
             container: ViewGroup?,
             savedInstanceState: Bundle?
     ): View? {
-        homeViewModel =
-                ViewModelProviders.of(this).get(HomeViewModel::class.java)
         val root = inflater.inflate(R.layout.fragment_home, container, false)
-        val textView: TextView = root.findViewById(R.id.text_home)
-        homeViewModel.text.observe(viewLifecycleOwner, Observer {
-            textView.text = it
-        })
         return root
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-
-        view.findViewById<View>(R.id.button_home).setOnClickListener {
-            val action = HomeFragmentDirections
-                    .actionHomeFragmentToHomeSecondFragment("From HomeFragment")
-            NavHostFragment.findNavController(this@HomeFragment)
-                    .navigate(action)
-        }
     }
 }
